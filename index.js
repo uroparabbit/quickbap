04d3e493b04aa5981318cfab8b1b21b9f0cbcb7e
uroparabbit <106389189+uroparabbit@users.noreply.github.com>
2024-12-17 19:12:51 +0900

    여유, 보통, 혼란, 최악 이미지 경로 수정 (. 포함)

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function updateStatus(cameras) {
     const count = cameras.findIndex(camera => camera <= 0) === -1 ? cameras.length : cameras.findIndex(camera => camera <= 0);
 
     const statusMap = [
-        { img: "/img/leisure.jpg", text: "대기가 거의 없으니 지금 고!" },
-        { img: "/img/moderate.jpg", text: "적당한 줄, 조금만 기다리면 식사 가능!" },
-        { img: "/img/confusing.jpg", text: "약간 혼잡, 많이 기다려야 할지도..." },
-        { img: "/img/worst.jpg", text: "지금은 급식 줄이 폭발 직전이니 가지 마세요!" }
+        { img: "./img/leisure.jpg", text: "대기가 거의 없으니 지금 고!" },
+        { img: "./img/moderate.jpg", text: "적당한 줄, 조금만 기다리면 식사 가능!" },
+        { img: "./img/confusing.jpg", text: "약간 혼잡, 많이 기다려야 할지도..." },
+        { img: "./img/worst.jpg", text: "지금은 급식 줄이 폭발 직전이니 가지 마세요!" }
     ];
 
     document.getElementById("img").src = statusMap[count].img
